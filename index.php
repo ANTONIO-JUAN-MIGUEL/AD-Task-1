42dfd8987433319f266893ced66ede5f132e86e7
MadmanMigz <202310011@fit.edu.ph>
2025-05-10 15:46:13 +0800

    feat (landing page): Added box.jpg to home page.

--- index.php
+++ index.php
@@ -14,6 +14,10 @@
     <p>Here you will learn the basic punches in boxing: jab, straight, hook, and uppercut. My favorite and current fighting style is the Slugger/Pressure fighter.</p>
     
     <p>Founded by Migz "Madman" Antonio, an amateur boxer with a record of 5 wins and no losses in the light heavyweight division. I have an upcoming amateur championship match sometime this year!</p>
+        
+    <div class="photo-container">
+        <img src="assets/img/Box.jpg" alt="Boxing Basics">
+    </div>
     
     <a href="page/index.php">Learn More About Boxing Basics</a>
 </body>
