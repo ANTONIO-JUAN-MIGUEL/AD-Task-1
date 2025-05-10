5201125b53eacebedd3e1838a77954a52d37d0ef
MadmanMigz <202310011@fit.edu.ph>
2025-05-10 15:40:37 +0800

    feat (landing page): Added landing page and description about me and boxing along with the file path.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Madman's Boxing Basics</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <?php include 'components/index.php'; ?>
+    
+    <h1>Welcome to Madman's Boxing Basics!</h1>
+
+    <p>Here you will learn the basic punches in boxing: jab, straight, hook, and uppercut. My favorite and current fighting style is the Slugger/Pressure fighter.</p>
+    
+    <p>Founded by Migz "Madman" Antonio, an amateur boxer with a record of 5 wins and no losses in the light heavyweight division. I have an upcoming amateur championship match sometime this year!</p>
+    
+    <a href="page/index.php">Learn More About Boxing Basics</a>
+</body>
+</html>
